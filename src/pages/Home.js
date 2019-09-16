85cdc9981eb7d71c6136bee909af014221466f07
Tam Lam <imtamtlam@gmail.com>
2019-09-16 15:11:16 +1000

    add parallax

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,44 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../components/Navigation";
 import Container from "react-bootstrap/Container";
+import Rellax from "rellax";
 
 class Home extends Component {
   state = {};
+  componentDidMount() {
+    var rellax = new Rellax(".rellax", {
+      speed: 1,
+      center: false,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false
+    });
+  }
   render() {
     return (
       <React.Fragment>
         <NavBar></NavBar>
         <Container className="mainBody nopadding" fluid="true">
-          <div className="object object--foreground shadow-lg"></div>
+          <div
+            className="rellax object object--foreground shadow-lg"
+            data-rellax-speed="-7"
+          ></div>
           <Container className="banner" fluid="true">
-            <div className="object object--background shadow-lg"></div>
-            <div className="object object--midground shadow-lg"></div>
+            <h1 className="rellax banner__h1 shadown-lg" data-rellax-speed="-5">
+              Hi,
+              <br />
+              I'm <span className="span--hightlight">Tam</span>,
+              <br /> developer.
+            </h1>
+            <div
+              className="rellax object object--background shadow-lg"
+              data-rellax-speed="1"
+            ></div>
+            <div
+              className="rellax object object--midground shadow-lg"
+              data-rellax-speed="-3"
+            ></div>
             <Container className="clipMask" fluid="true"></Container>
           </Container>
         </Container>
