76829049e13c3ec6128e6d5f74fd857f5f790c2c
Tam Lam <imtamtlam@gmail.com>
2019-09-16 13:13:34 +1000

    add objects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,10 @@ class Home extends Component {
       <React.Fragment>
         <NavBar></NavBar>
         <Container className="mainBody nopadding" fluid="true">
+          <div className="object object--foreground shadow-lg"></div>
           <Container className="banner" fluid="true">
+            <div className="object object--background shadow-lg"></div>
+            <div className="object object--midground shadow-lg"></div>
             <Container className="clipMask" fluid="true"></Container>
           </Container>
         </Container>
